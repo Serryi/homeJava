1b4837569709236a42248b3c00d8f2ec3f139a48
Serryi <138243073+Serryi@users.noreply.github.com>
2023-12-27 22:45:54 +0300

    Create Fish.java

---
+++ src/Fish.java
@@ -0,0 +1,20 @@
+public class Fish extends Animal {
+    public Fish(String name, String color) {
+        super(name, color,0);
+    }
+
+    public Fish(String name) {
+        this(name, null);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Skree!");
+    }
+
+    @Override
+    public void fly() {}
+
+    @Override
+    public void toGo() {}
+}
