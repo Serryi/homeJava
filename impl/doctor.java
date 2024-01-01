587d3d00a2ca96826482b97758c931176d828411
Serryi <138243073+Serryi@users.noreply.github.com>
2024-01-01 13:04:41 +0300

    Update doctor.java

--- impl/doctor.java
+++ impl/doctor.java
@@ -1,1 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
 
+import homeTask.Human;
+
+
+public class Doctor extends Human {
+
+    public Doctor(String name) {
+        super(name);
+    }
+
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    public void heal() {};
+
+    @Override
+    public void getIll() {};
+
+ 
+    public List<Doctor> getDoctors() {
+        List<Doctor> doctors = new ArrayList<>();
+        for (Doctor doctor : doctors) {
+            doctors.add((Doctor) doctor);
+        }
+        return doctors;
+    }
+    @Override
+    public String toString() {
+        return String.format("ФИО : %s", this.getName());
+    }
+}
