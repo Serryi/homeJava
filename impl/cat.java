89b752fce6000f9973188b7a5a3128d69111204d
Serryi <138243073+Serryi@users.noreply.github.com>
2024-01-01 13:02:10 +0300

    Update cat.java

--- impl/cat.java
+++ impl/cat.java
@@ -1,1 +1,25 @@
+import homeTask.Animal;
+import homeTask.Runnable;
 
+public class Cat extends Animal implements Runnable {
+    public Cat(String name, String color) {
+        super(name, color,4);
+    }
+
+    public Cat(String name) {
+        this(name, null);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Мяу!");
+    }
+
+    @Override
+    public void hunt() {}
+
+    @Override
+    public int getRunningSpeed() {
+        return 25;
+    }
+}
