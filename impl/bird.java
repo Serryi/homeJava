ad59357cdbd9e2e4fdbf7fc257aa03088351f5eb
Serryi <138243073+Serryi@users.noreply.github.com>
2024-01-01 13:00:22 +0300

    Update bird.java

--- impl/bird.java
+++ impl/bird.java
@@ -1,1 +1,25 @@
+import homeTask.Animal;
+import homeTask.Flyable;
 
+public class Bird extends Animal implements Flyable {
+    public Bird(String name, String color) {
+        super(name, color,2);
+    }
+
+    public Bird(String name) {
+        this(name, null);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Eeeaw!");
+    }
+
+    @Override
+    public void hunt() {}
+
+    @Override
+    public int getFlightSpeed() {
+        return 80;
+    }
+}
