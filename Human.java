328a51f2bfd93c263078bef3ff38e3cfe3286795
Serryi <138243073+Serryi@users.noreply.github.com>
2024-01-01 13:33:55 +0300

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,15 @@
+public abstract class Human implements Illable {
+    private String name;
+
+    @Override
+    public void getIll() {
+    }
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
