7c2f240e7585c60f3f5dc17caad67cf34b180304
Serryi <138243073+Serryi@users.noreply.github.com>
2024-01-01 13:10:32 +0300

    Update fish.java

--- impl/fish.java
+++ impl/fish.java
@@ -1,1 +1,25 @@
+import homeTask.Animal;
+import homeTask.Swimmable;
 
+public class Fish extends Animal implements Swimmable {
+    public Fish(String name, String color) {
+        super(name, color,0);
+    }
+
+    public Fish(String name) {
+        this(name, null);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("не издаю звуков");
+    }
+
+    @Override
+    public void hunt() {}
+
+    @Override
+    public int getSwimmingSpeed() {
+        return 45;
+    }
+}
