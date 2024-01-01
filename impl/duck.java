bf338a0e70e6a1fa9e9dbd5d93274587c7f54135
Serryi <138243073+Serryi@users.noreply.github.com>
2024-01-01 13:23:38 +0300

    Update duck.java

--- impl/duck.java
+++ impl/duck.java
@@ -1,1 +1,37 @@
+import homeTask.Animal;
+import homeTask.Flyable;
+import homeTask.Runnable;
+import homeTask.Swimmable;
 
+public class Duck extends Animal implements Flyable, Runnable, Swimmable {
+    public Duck(String name, String color) {
+        super(name, color,2);
+    }
+
+    public Duck(String name) {
+        this(name, null);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Кря");
+    }
+
+    @Override
+    public void hunt() {}
+
+    @Override
+    public int getFlightSpeed() {
+        return 60;
+    }
+
+    @Override
+    public int getRunningSpeed() {
+        return 10;
+    }
+
+    @Override
+    public int getSwimmingSpeed() {
+        return 3;
+    }
+}
