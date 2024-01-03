03a2db9599933685982d46c4b376010e2ccd2a2b
Serryi <138243073+Serryi@users.noreply.github.com>
2024-01-03 22:48:41 +0300

    Create Pinicilin.java

---
+++ HomeWork_3/Pinicilin.java
@@ -0,0 +1,7 @@
+package HomeWork_3;
+
+public class Pinicilin extends PharmacyComponent {
+    public Pinicilin(String name, String weight, int power) {
+        super(name, weight, power);
+    }
+}
